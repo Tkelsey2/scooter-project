8fbce0bf03e898132cde6c62f6bc5648d46824fd
Tkelsey2 <ty1erk@hotmail.com>
2022-10-21 12:10:47 +0100

    committing the scripting of the ScooterApp

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,57 @@
 class Scooter{
   // scooter code here
+    constructor(station, user){
+      this.station = station
+      this.user = user
+      this.serial = Math.floor(Math.random(1,100))
+      this.charge = Math.floor(Math.random(1,100))
+      this.isBroken = false
+      this.docked = true
+    }
+
+    rent(){
+      if (this.isBroken === false && this.charge > 20){
+        this.docked = false
+        console.log("Enjoy the ride!")
+      }
+      else if(this.charge <= 20){
+        console.log("Scooter battery is low, please charge.")
+      }
+      else{
+        console.log("Scooter is broken, please send a repair request.")
+        this.broken = true
+      }
+
+    }
+
+    dock(station){
+      this.station = station
+      if(this.station == undefined){
+        console.log("Docking station required")
+      } else {
+        this.docked = true
+        this.user = ""
+      }
+
+    }
+
+    async recharge() {
+      console.log('Starting charge');
+      
+      await new Promise(resolve => setTimeout(resolve, 2000)); // wait 2 seconds
+      this.charge = 100
+  
+      console.log('Charge complete');   
+  }
+
+  async requestRepair() {
+    console.log('Error: Repair Required');
+    
+    await new Promise(resolve => setTimeout(resolve,2000));
+    this.isBroken = false
+
+    console.log('Repairs Complete');   
+}
 }
 
 
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,49 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
-}
+  constructor(){
+    this.stations = [Manhattan, Brooklyn, Queens, Bronx, StatenIsland]
+    this.registeredUsers =[]
+  }
 
+  register(User){
+    for(let i = 0; i < this,this.registeredUsers.length; i++){
+      if(this.registeredUsers[i.username] === User.user){
+        console.log("Error user already registered")
+      }
+      else if(User.age <=17){
+        console.log("You are too young to register")
+      }
+      else {
+        this.registeredUsers.push(User)
+      }
+    }
+  }
+
+  logIn(username,password){
+    for(let i = 0; i < this.registeredUsers.length; i++){
+      if((this.registeredUsers[i.username] === username)&&(this.registeredUsers[i.password])=== password)
+      {
+        this.registeredUsers[i.loggedIn] = true
+        console.log("Welcome, you're all logged in")
+      }else{
+        console.log("Log In Failed, please try again...")
+      }
+    }
+  
+  }
+
+  addScooter(location, scooter){
+    Scooter.station = location
+    Scooter.station.push(scooter)
+  }
+
+
+
+  removeScooter(scooterToRemove){
+    
+
+  }
+
+}
 module.exports = ScooterApp
